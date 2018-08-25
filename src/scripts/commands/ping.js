e443be05b22168b65ba62c55a125672a28454e45
Ginma The Dev <37172966+Travels-of-Sutlr@users.noreply.github.com>
2018-08-25 09:35:26 +0700

    Add files via upload

--- src/scripts/commands/ping.js
+++ src/scripts/commands/ping.js
@@ -18,8 +18,36 @@ class Ping extends Command {
     }
 
     async run(msg, args, level) {
+        
+        let ping = new Date() - msg.createdAt;
 
-        await msg.channel.send(`Pong! Journey took ${new Date() - msg.createdAt}ms`);
+        await msg.channel.send({
+            
+            embed: { 
+
+                // I don't know anything about pings... lel
+                color: ping <= 50 ? 1044480 : ping <= 100 ? 61440 : ping <= 150 ? 15790080 : ping <= 500 ? 15728640 : 267386880,
+                fields: [
+
+                    {
+
+                        name: "\u{1F3D3} Ping",
+                        value: `${ping}ms`
+
+                    },
+
+                    {
+
+                        name: "\u2764 Heartbeat",
+                        value: `${this.client.ping}ms`
+                        
+                    }
+
+                ]
+
+            }
+
+        });
 
     }
 
